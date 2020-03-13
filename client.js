5886130f02d024cfebf2e0db46e83e6682df31ae
Leila Skinner <beileiwang@gmail.com>
2020-03-13 16:48:23 -0500

    add inputs to array

--- client.js
+++ client.js
@@ -1,1 +1,21 @@
-console.log("js");
+$(document).ready(init);
+
+const employees = [];
+
+function init() {
+  console.log("Ready");
+  $(".js-submit-employee").on("submit", addEmployee);
+}
+
+function addEmployee(event) {
+  event.preventDefault();
+  const employeeData = {
+    firstName: $(".js-input-first-name").val(),
+    lastName: $(".js-input-last-name").val(),
+    employeeId: $(".js-input-id").val(),
+    title: $(".js-input-title").val(),
+    annualSalary: parseInt($(".js-input-annual-salary").val())
+  };
+  employees.push(employeeData);
+  console.log(employees);
+}
